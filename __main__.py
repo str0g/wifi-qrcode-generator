55786b73ced166883e8f5a1ee3ca3bd8915d8840
Łukasz Buśko <busko.lukasz@pm.me>
2021-01-23 11:52:59 +0100

    refactor

--- __main__.py
+++ __main__.py
@@ -24,11 +24,12 @@
 from optparse import OptionParser
 from os.path import expanduser
 from io import BytesIO
-from shutil import copyfile
+import shutil
 import json
 import base64
 import tempfile
 import os
+import stat
 import logging
 import subprocess
 
@@ -36,68 +37,80 @@ import qrcode
 
 #pip install qrcode pillow
 
-FORMAT = '''\
-WIFI:T:{security_standard};S:{ssid};P:{password};H:{hidden};\
-'''
+logger = logging.getLogger('wifi-qrcode-generator')
 
 OUTFILE_TEX = 'out.tex'
 OUTFILE_PDF = OUTFILE_TEX.replace('.tex', '.pdf')
-logger = logging.getLogger('wifi-qrcode-generator')
 
 
-def get_profile(path: str):
-    with open(path) as fd:
-        cfg = json.load(fd)
-    logger.debug('profile loaded, filling qrcode template')
-    return FORMAT.format(**cfg), cfg
+class Generator():
+    SPECIAL = ['&', '%', '$' ,'#' ,'_' ,'{', '}', '~', '^' ,'\\']
+    FORMAT = '''\
+WIFI:T:{security_standard};S:{ssid};P:{password};H:{hidden};\
+'''
+    SECURITY_STANDARDS = ['WPA', 'WEP', '']
+    HIDDEN = ['true', '']
 
+    def __init__(self, ssid: str, passwd: str, template: str, security_standard:str = 'WPA', hidden: str =''):
+        if not security_standard in self.SECURITY_STANDARDS:
+            raise ValueError(f'Unknown security standard {security_standard} choose from {self.SECURITY_STANDARDS}')
+        if not hidden in self.HIDDEN:
+            raise ValueError(f'Wrong hidden value choose from {self.HIDDEN}')
 
-def get_qrcode(data: str, verbose: bool):
-    qr = qrcode.make(data)
-    io = BytesIO()
-    if verbose:
-        qr.save(OUTFILE_TEX.replace('.tex', '.png'))
-    qr.save(io, format='png')
-    io.seek(0)
-    b64 = base64.b64encode(io.getvalue()).decode('utf-8')
-    return {'qrcode': b64}
+        qrcode_string = f'WIFI:T:{security_standard};S:{ssid};P:{passwd};H:{hidden};'
+        template_dict = {
+            'ssid': self.replace_special(ssid),
+            'password': self.replace_special(passwd),
+            'qrcode': self.get_qrcode(qrcode_string),
+        }
+        self.template = template.format(**template_dict)
+        logger.debug(self.template)
 
+        self.path = ssid
 
-def get_template(path:str):
-    with open(path) as fd:
-        return fd.read()
+    def replace_special(self, data: str):
+        check = list(data)
+        return ''.join([f'\\{c}' if c in self.SPECIAL else c for c in check])
 
+    def get_qrcode(self, data: str):
+        qr = qrcode.make(data)
+        io = BytesIO()
+        if logger.level == logging.DEBUG:
+            qr.save(OUTFILE_TEX.replace('.tex', '.png'))
+        qr.save(io, format='png')
+        io.seek(0)
+        b64 = base64.b64encode(io.getvalue()).decode('utf-8')
+        return b64
 
-def build(data: str):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        logger.debug(f'created temporary directory {tmpdirname}')
+    def set_permissions(self, path: str, permissions=stat.S_IRUSR|stat.S_IWUSR):
+        os.chmod(path, permissions)
 
-        out_tex = os.path.join(tmpdirname, OUTFILE_TEX)
-        logger.debug(f'writing to {out_tex}')
-        with open(out_tex, 'w') as fd:
-            fd.write(data)
+    def save_file(self, filepath: str = OUTFILE_PDF, timeout: int = 1):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            logger.debug(f'created temporary directory {tmpdirname}')
 
-        logger.debug('executing build with pdflatex')
-        try:
-            proc = subprocess.run(['pdflatex', '--shell-escape', OUTFILE_TEX], stdout=subprocess.PIPE, timeout=1,
+            out_tex = os.path.join(tmpdirname, OUTFILE_TEX)
+            logger.debug(f'writing to {out_tex}')
+            with open(out_tex, 'w') as fd:
+                fd.write(self.template)
+
+            logger.debug('executing build with pdflatex')
+            tmp_pdf = os.path.join(tmpdirname, OUTFILE_PDF)
+            try:
+                proc = subprocess.run(['pdflatex', '--shell-escape', OUTFILE_TEX], stdout=subprocess.PIPE, timeout=timeout,
                             cwd=tmpdirname)
-            proc.check_returncode()
-            logger.debug(proc.stdout.decode('utf-8'))
-        except subprocess.TimeoutExpired as e:
-            logger.warning(proc.stdout.decode('utf-8'))
-            logger.warning(e)
-        except subprocess.CalledProcessError as e:
-            logger.warning(proc.stdout.decode('utf-8'))
-            logger.warning(e)
-
-        logger.debug(out)
-        copyfile(os.path.join(tmpdirname, OUTFILE_PDF), OUTFILE_PDF)
-
-
-def replace_special(data: str):
-    special = ['&', '%', '$' ,'#' ,'_' ,'{', '}', '~', '^' ,'\\']
-    check = list(data)
-    return ''.join([f'\\{c}' if c in special else c for c in check])
+                proc.check_returncode()
+                self.set_permissions(tmp_pdf)
+                logger.debug(proc.stdout.decode('utf-8'))
+                shutil.move(tmp_pdf, filepath)
+                return True
+            except subprocess.TimeoutExpired as e:
+                logger.warning(proc.stdout.decode('utf-8'))
+                logger.warning(e)
+            except subprocess.CalledProcessError as e:
+                logger.warning(proc.stdout.decode('utf-8'))
+                logger.warning(e)
+        return False
 
 
 def get_options():
@@ -120,29 +133,32 @@ def get_options():
     return options, args
 
 
+def get_profile(path: str):
+    with open(path) as fd:
+        cfg = json.load(fd)
+    logger.debug('profile loaded, filling qrcode template')
+    return cfg
+
+
+def get_template(path:str):
+    with open(path) as fd:
+        return fd.read()
+
+
 def set_logger(verbose: bool):
     logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
 
 
 if __name__ == "__main__":
-    #
     (options, args) = get_options()
     set_logger(options.verbose)
-    #
-    profile, cfg = get_profile(options.input)
-    #
-    logger.debug('making things with special characters')
-    cfg['ssid'] = replace_special(cfg['ssid'])
-    cfg['password'] = replace_special(cfg['password'])
 
-    out = {}
-    out.update(cfg)
-    out.update(get_qrcode(profile, options.verbose))
-    logger.debug('data for template has been prepared')
-    logger.debug(out)
+    cfg = get_profile(options.input)
+    template = get_template(options.template)
 
-    template = get_template(options.template).format(**out)
-    logger.debug(template)
-    build(template)
-
-    logger.info(f'Created {OUTFILE_PDF}')
+    gen = Generator(cfg['ssid'], cfg['password'], template)
+    outfile = f"{cfg['ssid']}.pdf"
+    if gen.save_file(outfile):
+        logger.info(f'Created {outfile}')
+    else:
+        logger.warning(f'Something went wrong during {outfile} creation')
